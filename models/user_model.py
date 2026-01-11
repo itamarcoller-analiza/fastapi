371c9d784e1a3abdbf59f1e10e9db49f2084cec7
Itamar Coller <itamarcoller@analiza-college.co.il>
2026-01-11 17:20:28 +0200

    adjusting optional

--- models/user_model.py
+++ models/user_model.py
@@ -6,4 +6,4 @@ class User(BaseModel):
     id: Annotated[int, None] = None
     name: Annotated[str, Field(strict=True, min_length=1, max_length=50)]
     email: Annotated[EmailStr, Field(strict=True)]
-    age: Annotated[int, Optional(Field(strict=True, ge=0, le=120))] = None
+    age: Optional[Annotated[int, Field(strict=True, ge=0, le=120)]] = None
