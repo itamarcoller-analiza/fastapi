65ca1a644627684fbabb0890617b189c60644fd8
Itamar Coller <itamarcoller@analiza-college.co.il>
2026-01-11 15:32:45 +0200

    adding api and model

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import random
 from fastapi import FastAPI
-import uvicorn
+from routes import user_route
 
 
 app = FastAPI()
 
+app.include_router(user_route.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/chat")
+async def chat():
+    chat_response = ['Hello! How can I assist you today?','Sure! What would you like to know?','I am here to help you with any questions you have.','Feel free to ask me anything!',
+                     'What topic are you interested in discussing?','I can provide information on a wide range of subjects.','Let me know how I can assist you further.','Is there something specific you would like to talk about?',
+                     'I am ready to help you with your inquiries.','Thank you for reaching out! How can I assist you?']
+    return {"responses": chat_response.index(chat_response[random.randint(0, len(chat_response)-1)])}
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, log_level="info")
---
+++ models/user_model.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import Annotated, Optional
+
+
+class User(BaseModel):
+    id: Annotated[int, None] = None
+    name: Annotated[str, Field(strict=True, min_length=1, max_length=50)]
+    email: Annotated[EmailStr, Field(strict=True)]
+    age: Annotated[int, Optional(Field(strict=True, ge=0, le=120))] = None
